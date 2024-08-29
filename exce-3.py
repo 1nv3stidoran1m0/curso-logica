d03692e6f082d94ca202e06f06af12b417e4d7f4
1nv3stidoran1m0 <enzoflm03@gmail.com>
2024-08-29 20:59:11 -0300

    Create exce-3.py

---
+++ exce-3.py
@@ -0,0 +1,5 @@
+#33. Letra de música - Faça um programa que mostre na tela uma letra de música qu você gosta (proibidoletras do Justin Bieber).
+  print("Is this the real life? Is this just fantasy?")
+  print("Caught in a landslide, no escape from reality")
+  print("Open your eyes, look up to the skies and see")
+  print("I'm just a poor boy, I need no sympathy")
