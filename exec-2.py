8f61b83c699bb8cd4f684db3cceb3f3fb45c41ae
1nv3stidoran1m0 <enzoflm03@gmail.com>
2024-08-29 20:56:54 -0300

    Create exec-2.py

---
+++ exec-2.py
@@ -0,0 +1,6 @@
+#2. Etiqueta - Elabore um programa que escreve seu nome na primeira linha, o estado em que você mora na segunda, e seu município na terceira.
+
+
+print("enzo")
+print("rio de janeiro")
+print("sao de meriti")
